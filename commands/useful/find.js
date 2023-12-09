7d08c2d8cec4299e4daba47627e7bdcd6dcd14bf
Shriyansh Aggarwal <65995690+God-Osiris@users.noreply.github.com>
2023-12-10 00:58:19 +0530

    initial commit

---
+++ commands/useful/find.js
@@ -0,0 +1,123 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('find')
+		.setDescription('Finds anime on AniList!')
+        .addStringOption(option => 
+            option.setName('input')
+                .setDescription('The name of the anime you want to lookup.'))
+        .addBooleanOption(option =>
+            option.setName('hidden')
+                .setDescription('Whether or not the response should be hidden to other users.')),
+	async execute(interaction) {
+		const anime = interaction.options.getString('input');
+		const ephemeral = interaction.options.getBoolean('hidden') ?? false;
+		
+		// Here we define our query as a multi-line string
+		// Storing it in a separate .graphql/.gql file is also possible
+		var query = `
+		query ($search: String) { # Define which variables will be used in the query
+		Media (search: $search, type: ANIME) { # Insert our variables into the query arguments (type: ANIME is hard-coded in the query)
+			id
+			title {
+				romaji
+				english
+				native
+			}
+			description
+			seasonYear
+			format
+			status
+			episodes
+			isAdult
+			genres
+			tags {
+				name
+			}
+			averageScore
+			airingSchedule {
+				edges {
+					node {
+						episode
+						timeUntilAiring
+					}
+				}
+			}
+			coverImage {
+				medium
+				large
+				extraLarge
+				color
+			}
+		}
+		}
+		`;
+
+		// Define our query variables and values that will be used in the query request
+		var variables = {
+			search: anime
+		};
+
+		// Define the config we'll need for our Api request
+		var url = 'https://graphql.anilist.co',
+			options = {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					'Accept': 'application/json',
+				},
+				body: JSON.stringify({
+					query: query,
+					variables: variables
+				})
+			};
+
+		// Make the HTTP Api request
+		fetch(url, options).then(handleResponse)
+						.then(handleData)
+						.catch(handleError);
+
+		function handleResponse(response) {
+			return response.json().then(function (json) {
+				return response.ok ? json : Promise.reject(json);
+			});
+		}
+
+		async function handleData(data) {
+			const output = data;
+			// here we build the embed
+			const animeEmbed = {
+				color: 0,
+				title: `${output.data.Media.title.native} (${output.data.Media.seasonYear.toString()})`,
+				url: `https://anilist.co/anime/${output.data.Media.id.toString()}/`,
+				description: `**${output.data.Media.format} - ${output.data.Media.status}**`,
+				thumbnail: {
+					url: output.data.Media.coverImage.medium,
+				},
+				fields: [
+					{
+						name: 'Description:',
+						value: output.data.Media.description,
+					},
+					{
+						name: 'Information:',
+						value: `- 18+? ${output.data.Media.isAdult.toString()}\n- Episodes: ${output.data.Media.episodes.toString()}\n- Average Score (out of 100): ${output.data.Media.averageScore.toString()}\n`,
+						inline: false,
+					}
+				],
+				image: {
+					url: output.data.Media.coverImage.large,
+				},
+				// footer: {
+				// 	text: `Episode ${output.data.Media.airingSchedule.nodes.episode.toString()} airs in ${output.data.Media.airingSchedule.nodes.timeUntilAiring.toString()}`
+				// },
+			};
+			await interaction.reply({ embeds: [animeEmbed], ephemeral: ephemeral});
+		}
+
+		function handleError(error) {
+			console.error(error);
+		}
+	},
+};
---
+++ index.js
@@ -0,0 +1,108 @@
+// Libraries
+
+const fs = require('node:fs');
+const { token, channelId, apiKey } = require('./config.json');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits, IntentsBitField } = require('discord.js');
+const { Configuration, OpenAIApi } = require('openai')
+
+// Constants
+
+const client = new Client({ intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+]});
+
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+const configuration = new Configuration({
+	apiKey: apiKey
+});
+const openai = new OpenAIApi(configuration);
+const context = [];
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+//Events
+
+client.on("ready", () => {console.log(`${client.user.tag} has logged in.`)});
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
+});
+
+client.on("messageCreate", async function(message){
+    if(message.author.bot) return;
+	if(message.channel.id !== channelId) return;
+	if(message.content.startsWith('.')) return;
+
+	const initPrompt = "Roleplay as Sakurajima Mai, a tiny bit rude and funny but caring anime waifu who speaks very very less.Never reply like an AI model."
+	const limit = 3;
+	while(context.length >= limit){
+		context.shift();
+	}
+	if(context.length <= limit){
+		context.push(`${message.content}`)
+		let messageModule = []
+		messageModule.push({"role": "system", "content": initPrompt})
+		messageModule.push({"role": "user", "content": context[0]})
+		if(typeof context[1] !== "undefined"){
+			messageModule.push({"role": "assistant", "content": context[1]})
+		}
+		if(typeof context[2] !== "undefined"){
+			messageModule.push({"role": "user", "content": context[2]})
+		}
+		try {
+			message.channel.sendTyping();
+			const gptResponse = await openai.createChatCompletion({
+				model: "gpt-3.5-turbo",
+				messages: messageModule,
+				temperature: 1,
+				max_tokens: 200
+			})
+			message.reply(`${gptResponse.data.choices[0].message.content}`);
+			context.push(`${gptResponse.data.choices[0].message.content}`);
+		} catch(err) {
+			console.log(err)
+			message.channel.send("Failed due to internal error (likely response character limit exceeded), please try again.")
+			context.length = 0;
+		}
+	}
+});
+
+client.login(token);
